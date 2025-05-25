b2a3b85cb8af601dbcbfd659fb88054ef7f69b46
Nana Ama Sarpong <nanaamasarpong824@gmail.com>
2025-05-25 09:34:27 +0000

    3 value arguments

---
+++ 3-value_argument.js
@@ -0,0 +1,7 @@
+const arg = process.argv[2]; 
+
+if (arg != null) {
+  console.log(arg);
+} else {
+  console.log('No argument');
+}
--- 3-value_arguments.js
+++
@@ -1,7 +0,0 @@
-const arg = process.argv[2];
-
-if (arg != null) {
-  console.log(arg);
-} else {
-  console.log('No argument');
-}
